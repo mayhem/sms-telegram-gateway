6678f710d7609ca08e09d9b8e687984e3e5f7154
Robert Kaye <rob@musicbrainz.org>
2017-04-05 22:53:44 +0000

    Not fully debugged yet, but should be much better

--- sms-fona.py
+++ sms-fona.py
@@ -23,7 +23,6 @@ class SMSModem(threading.Thread):
         self.ser = None
         self.bot = None
         self.exit = False
-        self.new_messages = False
         self.in_q = Queue()
 
     def open(self, device, baud_rate):
@@ -39,15 +38,6 @@ class SMSModem(threading.Thread):
             return False
 
         return True
-
-    def decode_msg(self, msg):
-        try:
-            msg = msg.strip()
-            return msg.decode('hex').decode('iso-8859-1',errors='replace').encode('utf-8')
-        except TypeError:
-            pass
-
-        return msg.decode('iso-8859-1',errors='replace').encode('utf8')
 
     def next_message(self):
         try:
@@ -89,56 +79,26 @@ class SMS(object):
 
     def __init__(self):
         self.modem = SMSModem()
+        self.new_messages = False
 
     def set_bot(self, bot):
         self.bot = bot
 
-    def process_messages(self):
-        if not self.bot:
-            log.info("Not connected to Telegram, skipping catch up.")
-            return
+    def decode_message(self, msg):
 
-        stored_cmds = []
-        new_messages = True
+        # this is just a test to see if it is all hex
+        try:
+            msg = msg.strip()
+            msg.decode('hex')
+        except TypeError:
+            return unicode(msg.decode('iso-8859-1',errors='replace'))
 
-        self.ser.write('AT+CMGF=1\n')
-        self.wait_for()
+        out = ""
+        while len(msg):
+            out += (msg[2:4]+msg[0:2]).decode('hex').decode('utf-16')
+            msg = msg[4:]
 
-        self.ser.write('AT+CMGL="ALL"\n')
-        self.new_messages = False
-        while new_messages:
-            new_messages = False
-            while True:
-                line = self.ser.readline()
-                if not line:
-                    log.error("timeout, process messages")
-                    break
-                line = line.strip()
-                log.info("r>%s" % line)
-
-                if line.startswith("OK"):
-                    break
-
-                if line.startswith("+CMGL:"):
-                    data = line.split(',')
-                    cmd, index = data[0].split(' ')
-                    sender = data[2][1:-1]
-                    dt = data[4][1:] + " " + data[5][0:-1]
-                    msg = self.decode_msg(self.ser.readline())
-                    log.info("S> %s, %s, %s: %s" % (index, sender, dt, msg))
-
-                    try:
-                        bot.sendMessage(chat_id=config.CHAT_ID, text="%s @ %s\n%s" % (sender, dt, msg))
-                        stored_cmds.append('AT+CMGD=%s\n' % index)
-                    except telegram.TelegramError as e:
-                        log.error("Cannot send message to telegram." + str(e))
-
-                if line.startswith("+CMTI:"):
-                    new_messages = True
-
-            for cmd in stored_cmds:
-                self.ser.write(cmd)
-                self.wait_for()
+        return unicode(out)
 
     def send_message(self, msg):
         self.modem.send_message(msg)
@@ -170,18 +130,18 @@ class SMS(object):
         return False
 
     def handle_telegram_message(self, msg):
-        p = re.compile('^6[0-9]{8}:')
+        p = re.compile('^\+[0-9]+:')
         m = p.match(msg)
         if m:
+            num = m.group()[:-1]
             log.info("T>" + msg)
-            out = 'AT+CMGS="+34' + m.group()[:-1] + '"\n'
-            self.ser.write(out.encode('utf-8'))
+            out = 'AT+CMGS="' + num + '"'
+            self.send_message(out.encode('utf-8'))
             self.wait_for(">")
-            out = ('%s' % msg[11:]) + chr(26)
-            self.ser.write(out.encode('utf-8'))
+            out = ('%s' % msg[14:]) + chr(26)
+            self.send_message(out.encode('utf-8'))
             log.info("T> sent")
         else:
-            # TODO: Split messages 
             if msg != "/start":
                 bot.sendMessage(config.CHAT_ID, text="You suck. Invalid message format, yo! Use <ES mobile number>: <message>")
                 bot.sendMessage(config.CHAT_ID, text="you said: '%s'" % msg)
@@ -195,30 +155,49 @@ class SMS(object):
         self.send_message('AT+CMGF=1')
         self.wait_for()
 
-        self.send_message('AT+CMGL="ALL"')
-        self.wait_for()
-        log.info("OK!")
-
-    def crappy_crap(self):
-
         next_id = None
-        line = ""
         while True:
+            try:
+                updates = bot.getUpdates(offset=next_id)
+            except telegram.TelegramError as e:
+                log.error("Telegram error: ", str(e))
+            except Exception as e:
+                log.error("general error: ", str(e))
+            for u in updates:
+                self.handle_telegram_message(u.message.text)
+                next_id = u.update_id + 1
 
-            # If we haven't started receiving a line (idle), do event loop stuff
-            if not line:
-                if self.new_messages:
-                    self.process_messages()
+            if not self.new_messages:
+                continue
 
-                try:
-                    updates = bot.getUpdates(offset=next_id)
-                except telegram.TelegramError as e:
-                    print "Telegram error: ", str(e)
-                except Exception as e:
-                    print "general error: ", str(e)
-                for u in updates:
-                    self.handle_telegram_message(u.message.text)
-                    next_id = u.update_id + 1
+            self.send_message('AT+CMGL="ALL"')
+            stored_cmds = []
+            while True:
+                msg = self.next_message()
+                if not msg:
+                    continue
+
+                if msg.startswith("+CMGL:"):
+                    data = msg.split(',')
+                    cmd, index = data[0].split(' ')
+                    sender = data[2][1:-1]
+                    dt = data[4][1:] + " " + data[5][0:-1]
+                    msg = self.decode_message(self.next_message())
+                    log.info("S> %s, %s, %s: %s" % (index, sender, dt, msg))
+
+                    try:
+                        bot.sendMessage(chat_id=config.CHAT_ID, text="%s @ %s\n%s" % (sender, dt, msg))
+                        stored_cmds.append('AT+CMGD=%s\n' % index)
+                    except telegram.TelegramError as e:
+                        log.error("Cannot send message to telegram." + str(e))
+
+                if msg.startswith("OK"):
+                    for cmd in stored_cmds:
+                        self.send_message(cmd)
+                        self.wait_for()
+                    self.new_messages = False
+                    break
+
 
 logging.basicConfig(filename=os.path.join(os.path.dirname(os.path.realpath(__file__)), 'gateway.log'),level=logging.INFO)
 log = logging
